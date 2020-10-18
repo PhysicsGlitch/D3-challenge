c2a0f9eaaf0113b15c1b92fc1f57023fd7513063
Jason <stanghelle@gmail.com>
2020-10-18 17:42:03 -0500

    Added d3 event clicks to labels to make bold, now need to tie x and y axis together.

--- D3_data_journalism/assets/js/bonus-app.js
+++ D3_data_journalism/assets/js/bonus-app.js
@@ -11,6 +11,99 @@ var svg_bonus = d3.select("#bonus")
     .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
+    
+// Add Axis Labels
+
+var labelGroups = svg_bonus.append("g");
+
+// https://bl.ocks.org/d3noob/23e42c8f67210ac6c678db2cd07a747e
+    
+var ylabel1 = labelGroups.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 60 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("font-family", "Arial")
+      .attr("font-size", "20px")
+      .attr("font-weight", "bold")
+      .attr("value", "ylabel1")
+      .style("text-anchor", "middle")
+      .text("yLabel1");
+
+var ylabel2 = labelGroups.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 30 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("font-family", "Arial")
+      .attr("font-size", "20px")
+      .attr("font-weight", "lighter")
+      .attr("value", "ylabel2")
+      .style("text-anchor", "middle")
+      .text("yLabel2");
+    
+var ylabel3 = labelGroups.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("font-family", "Arial")
+      .attr("font-size", "20px")
+      .attr("font-weight", "lighter")
+      .attr("value", "ylabel3")
+      .style("text-anchor", "middle")
+      .text("yLabel3");
+    
+var xlabel1 = labelGroups.append("text")
+      .attr("y", height + 60)
+      .attr("x", 30 + (width / 2 ))
+      .attr("dx", "1em")
+      .attr("font-family", "Arial")
+      .attr("font-size", "20px")
+      .attr("font-weight", "bold")
+      .attr("value", "xlabel1")
+      .style("text-anchor", "middle")
+      .text("xLabel1");
+
+var xlabel2 = labelGroups.append("text")
+      .attr("y", height + 100)
+      .attr("x", 30 + (width / 2 ))
+      .attr("dx", "1em")
+      .attr("font-family", "Arial")
+      .attr("font-size", "20px")
+      .attr("font-weight", "lighter")
+      .attr("value", "xlabel2")
+      .style("text-anchor", "middle")
+      .text("xLabel2");
+    
+var xlabel3 = labelGroups.append("text")
+      .attr("y", height + 140)
+      .attr("x", 30 + (width / 2 ))
+      .attr("dx", "1em")
+      .attr("font-family", "Arial")
+      .attr("font-size", "20px")
+      .attr("font-weight", "lighter")
+      .attr("value", "xlabel3")
+      .style("text-anchor", "middle")
+      .text("xLabel3");
+
+// Append click events to each label by selecting the svg element, text and then binding it with d3.select(this)
+    
+var chosenXaxis = "xLabel1"
+var chosenYaxis = "ylabel1"
+
+labelGroups.selectAll("text")
+         .on('click', function () {
+          var clicked_axis = d3.select(this).attr("value");
+                 
+          if (clicked_axis != chosenXaxis) {
+              chosenXaxis = clicked_axis;
+              console.log(chosenXaxis);
+              d3.selectAll("text").style("font-weight", "lighter")
+              d3.select(this).style("font-weight", "bold");
+          }
+          }); 
+  
 
 //Read the data
 d3.csv("assets/data/data.csv").then(function(data) {
@@ -20,10 +113,8 @@ d3.csv("assets/data/data.csv").then(function(data) {
     data.income = +data.income;
     data.smokes = +data.smokes;
     data.healthcare = +data.healthcare;
-    return data;
-  });
 
-  // Add X axis
+// Add X axis
   var x = d3.scaleLinear()
     .domain([8, 25])
     .range([ 0, width ]);
@@ -37,7 +128,6 @@ d3.csv("assets/data/data.csv").then(function(data) {
     .range([ height, 0]);
   svg_bonus.append("g")
     .call(d3.axisLeft(y));
-
   // Add dots: I declared it as a variable, scatter dots to make it a bit easier to see how I appended both circles and then dots to the poits. 
  var scatter_dots = svg_bonus.selectAll("g")
                 .data(data)
@@ -47,8 +137,8 @@ d3.csv("assets/data/data.csv").then(function(data) {
 // Add circles to dots
 scatter_dots.append("circle")
   .attr("class", "dot")
-  .attr("cx", function(d) { return x(d.poverty); })
-  .attr("cy", function(d) { return y(d.obesity); })
+  .attr("cx", function(d) { return x(d.data.poverty); })
+  .attr("cy", function(d) { return y(d.data.obesity); })
   .attr("r", 12)
   .style("fill", "#69b3a2");
 
@@ -56,79 +146,14 @@ scatter_dots.append("circle")
 scatter_dots.append("text")
     .text(function(d) {
     return d.abbr; })
-  .attr("x", function(d) { return x(d.poverty); })
-  .attr("y", function(d) { return y(d.obesity); })
+  .attr("x", function(d) { return x(data.poverty); })
+  .attr("y", function(d) { return y(data.obesity); })
   .attr("font-family", "Arial")
   .attr("font-size", "9px")
   .attr("text-anchor", "middle")
-  .attr("fill", "white");
+  .attr("fill", "white");    
     
-// Add Axis Labels
+      
 
-
-// https://bl.ocks.org/d3noob/23e42c8f67210ac6c678db2cd07a747e
-    
-var ylabel1 = svg_bonus.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 60 - margin.left)
-      .attr("x",0 - (height / 2))
-      .attr("dy", "1em")
-      .attr("font-family", "Arial")
-      .attr("font-size", "20px")
-      .attr("font-weight", 550)
-      .style("text-anchor", "middle")
-      .text("Label 1");
-
-var ylabel2 = svg_bonus.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 30 - margin.left)
-      .attr("x",0 - (height / 2))
-      .attr("dy", "1em")
-      .attr("font-family", "Arial")
-      .attr("font-size", "20px")
-      .attr("font-weight", 550)
-      .style("text-anchor", "middle")
-      .text("Label 2");
-    
-var ylabel3 = svg_bonus.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x",0 - (height / 2))
-      .attr("dy", "1em")
-      .attr("font-family", "Arial")
-      .attr("font-size", "20px")
-      .attr("font-weight", 550)
-      .style("text-anchor", "middle")
-      .text("Label 3");
-    
-var xlabel1 = svg_bonus.append("text")
-      .attr("y", height + 60)
-      .attr("x", 30 + (width / 2 ))
-      .attr("dx", "1em")
-      .attr("font-family", "Arial")
-      .attr("font-size", "20px")
-      .attr("font-weight", 550)
-      .style("text-anchor", "middle")
-      .text("Label 1");
-
-var xlabel2 = svg_bonus.append("text")
-      .attr("y", height + 100)
-      .attr("x", 30 + (width / 2 ))
-      .attr("dx", "1em")
-      .attr("font-family", "Arial")
-      .attr("font-size", "20px")
-      .attr("font-weight", 550)
-      .style("text-anchor", "middle")
-      .text("Label 2");
-    
-var xlabel2 = svg_bonus.append("text")
-      .attr("y", height + 140)
-      .attr("x", 30 + (width / 2 ))
-      .attr("dx", "1em")
-      .attr("font-family", "Arial")
-      .attr("font-size", "20px")
-      .attr("font-weight", 550)
-      .style("text-anchor", "middle")
-      .text("Label 3");
+    });
 });;
-    
